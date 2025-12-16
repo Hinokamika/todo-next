2abc614225818cde80dc293906f0c8f620117e15
Hinokamika <manh.ndm2506@gmail.com>
2025-12-17 00:17:58 +0700

    first commit

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="border-b border-b-gray-700 dark:bg-[#112117]">
+      <div className="px-8 py-4 flex items-start">
+          <div className="font-bold">
+              Todo App
+          </div>
+      </div>
+    </div>
+  );
+}
